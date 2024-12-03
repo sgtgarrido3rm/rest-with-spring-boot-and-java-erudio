10013fc0dae69bb0840cc1cdb5e2931f1250a96a
sgtgarrido3rm <sgtgarrido3rm@gmail.com>
2024-12-02 21:37:24 -0300

    Criado todo o CRUD, e testes no browser e Postman

---
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,80 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+
+@Service
+public class PersonServices {
+	
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public List<Person> findAll() {
+		
+		logger.info("Finding all people!");
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;		
+	}
+		
+	public Person findById(String id) {
+		
+		logger.info("Finding one person!");
+
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Olavo");
+		person.setLastName("Garrido");
+		person.setAddress("Viamão - Rio Grande do Sul - Brasil");
+		person.setGender("Male");
+		return person;
+	}
+	
+	public Person create(Person person) {
+
+		logger.info("Creating one person!");
+		//conn bd
+		return person;
+	}
+
+	public Person update(Person person) {
+
+		logger.info("Updating one person!");
+		//conn bd
+		return person;
+	}
+
+	public void delete(String id) {
+
+		logger.info("Deleting one person!");
+		//conn bd
+	}
+
+	private Person mockPerson(int i) {
+		
+		String gender;
+		if (i % 2 == 0) {
+			gender = "Male";
+		} else {
+			gender =  "Female";
+		}
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brasil " + i);
+		person.setGender(gender);
+		return person;
+	}
+}
