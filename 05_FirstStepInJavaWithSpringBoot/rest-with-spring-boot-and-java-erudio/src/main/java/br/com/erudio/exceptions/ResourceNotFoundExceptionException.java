afde42b56b2f383b5bcf7c4c97688d3cbccdab9a
sgtgarrido3rm <sgtgarrido3rm@gmail.com>
2024-12-02 23:54:17 -0300

    Substituição dos Mocks, Hibernate ORM / JPA, Testando CRUD aplicação

--- 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exceptions/ResourceNotFoundExceptionException.java
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exceptions/ResourceNotFoundExceptionException.java
@@ -3,12 +3,12 @@ package br.com.erudio.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UnsupportedMathOperationException extends RuntimeException{
-	
-	public UnsupportedMathOperationException(String ex) {
-		super(ex);
-	}
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundExceptionException extends RuntimeException{
 	
 	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundExceptionException(String ex) {
+		super(ex);
+	}	
 }
--- 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -5,76 +5,67 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.erudio.exceptions.ResourceNotFoundExceptionException;
 import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
 
 
 @Service
 public class PersonServices {
 	
-	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonRepository repository;
 	
 	public List<Person> findAll() {
 		
 		logger.info("Finding all people!");
-		List<Person> persons = new ArrayList<>();
-		for (int i = 0; i < 8; i++) {
-			Person person = mockPerson(i);
-			persons.add(person);
-		}
-		return persons;		
+		
+		return repository.findAll();		
 	}
 		
-	public Person findById(String id) {
+	public Person findById(Long id) {
 		
 		logger.info("Finding one person!");
-
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Olavo");
-		person.setLastName("Garrido");
-		person.setAddress("Viamão - Rio Grande do Sul - Brasil");
-		person.setGender("Male");
-		return person;
+		
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundExceptionException("No records found for this ID"));
 	}
 	
 	public Person create(Person person) {
 
 		logger.info("Creating one person!");
 		//conn bd
-		return person;
+		return repository.save(person);
 	}
 
 	public Person update(Person person) {
 
 		logger.info("Updating one person!");
 		//conn bd
-		return person;
+		
+		var entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundExceptionException("No records found for this ID"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(person);
 	}
 
-	public void delete(String id) {
+	public void delete(Long id) {
 
 		logger.info("Deleting one person!");
 		//conn bd
-	}
-
-	private Person mockPerson(int i) {
 		
-		String gender;
-		if (i % 2 == 0) {
-			gender = "Male";
-		} else {
-			gender =  "Female";
-		}
-		
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Person name " + i);
-		person.setLastName("Last name " + i);
-		person.setAddress("Some address in Brasil " + i);
-		person.setGender(gender);
-		return person;
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundExceptionException("No records found for this ID"));
+		repository.delete(entity);
 	}
 }
